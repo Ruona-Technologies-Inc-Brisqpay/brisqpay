f293e1b02bfae8e0ece800ec8d179e55b3f9d0f4
Femi Adegbite <adegbitefemisason@gmail.com>
2026-06-22 17:36:45 -0600

    coming soon page

---
+++ theme.ts
@@ -0,0 +1,8 @@
+import { createTheme } from "@mantine/core";
+
+export const theme = createTheme({
+  primaryColor: "indigo",
+  fontFamily:
+    "var(--font-geist-sans), -apple-system, BlinkMacSystemFont, sans-serif",
+  defaultRadius: "md",
+});
